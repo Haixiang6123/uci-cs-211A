e726cfde4a4a69cf985933a80c55153b9fe0e20f
Haixiang <haixiang6123@gmail.com>
2019-10-29 16:42:10 -0700

    Add part1

---
+++ Assignment2/part1.m
@@ -0,0 +1,36 @@
+%% a
+rows = 512;
+[x,y] = meshgrid(1:rows, 1:rows);
+img = sin(0.2 * x) + sin(0.3 * x) + cos(0.4 * x) + sin(sqrt(x .* x + y .* y) * 0.15) + sin(sqrt(x .* x + y .* y) * 0.35);
+imshow(img);
+
+%% a step 3
+fftImg = fftshift(fft2(img));
+% Show the magnitude and phase of DFT for this image
+phase = angle(fftImg);
+amplitude = abs(fftImg);
+
+figure;
+imshow(log(amplitude),[]);
+title("Log amplitude");
+
+figure;
+imshow(unwrap(phase), []);
+title("Phase");
+
+%% a step 4
+% Multiply the magnitude of DFT with 2
+mul = ifftshift(mag) * 2;
+% Calculate the inverse Discrete Fourier Transform
+ifftImg = ifft2(mul);
+figure
+imshow(ifftImg);
+
+%% b
+img = im2double(imread('./images/Cross.jpg'));
+
+dft = fft2(img);
+fftImg = fftshift(dft);
+
+figure
+imshow((fftshift(fftImg)));
